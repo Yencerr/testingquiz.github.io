a939390527fcc8622d4c12b9d368913f35759e1c
Timothy L <talmabertt@gmail.com>
2020-03-05 23:01:49 -0600

    changes made to all pages

---
+++ script/scorescript.js
@@ -0,0 +1,15 @@
+
+// Overall elements to the page //
+var start = document.getElementById("start");
+var quiz = document.getElementById("quiz");
+var question = document.getElementById("question");
+var choiceA = document.getElementById("A");
+var choiceB = document.getElementById("B");
+var choiceC = document.getElementById("C");
+var counter = document.getElementById("counter");
+var timeGauge = document.getElementById("timeGauge");
+var progress = document.getElementById("progress");
+var scoreDiv = document.getElementById("scoreContainer");
+
+
+
